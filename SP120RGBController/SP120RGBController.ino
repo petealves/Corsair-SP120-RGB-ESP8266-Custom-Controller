51691e5b62c833aae6c9eec4937aa5fcd520303e
Pedro Alves <pedroalves-1995@hotmail.com>
2019-05-15 18:27:42 +0100

    Now saving changes to EEPROM to restore colors on PC Restart.

--- SP120RGBController/SP120RGBController.ino
+++ SP120RGBController/SP120RGBController.ino
@@ -4,9 +4,10 @@
 #include <ESP8266mDNS.h>
 #include <FastLED.h>
 #include <ArduinoOTA.h>
+#include <EEPROM.h>
 
 #define LEDS_PER_FAN 1
-#define NR_FANS 3
+#define NR_FANS 4
 #define NUM_LEDS (NR_FANS * LEDS_PER_FAN)
 #define DATA_PIN 2
 #define LED_TYPE UCS1903
@@ -15,8 +16,8 @@ int FRAMES_PER_SECOND = 60;
 
 CRGB leds[NUM_LEDS];
 
-#define ssid      "YOUR_SSID"       // WiFi SSID
-#define password  "YOUR_PASSWORD"  // WiFi password
+#define ssid      "SSID"       // WiFi SSID
+#define password  "PASSWORD"  // WiFi password
 
 
 ESP8266WebServer server ( 80 );
@@ -28,7 +29,7 @@ int bpmEffect = 0;
 int status_on = 1;
 int brightness = 255;
 int brightnessPercentage = (brightness*100)/255;
-String color = "#FFFFFF";
+String color = "FF0000";
 int r, g, b = 0;
 String effect = "";
 
@@ -42,6 +43,7 @@ String getPage(){
   page += "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>";
   page += "<link href='https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css' rel='stylesheet'> <script src='https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js'></script>";
   page += "<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>";
+  page += "<script src='https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.js' integrity='sha256-y1h79SSb3icyBC/BrLCzoexQZAOGwlvoAr85biY4QGw=' crossorigin='anonymous'></script>";
   page += "<title>ESP8266 Corsair SP120 RGB Controller</title></head><body style='background-color:#212121'>";
   page += "<div class='container-fluid' style='width:70%'>";
   page +=       "<br>";
@@ -50,7 +52,7 @@ String getPage(){
   page +=       "<h1 style='color:white;'>ESP8266 Corsair SP120 RGB Controller</h1>";
   page +=       "<div class='row'>";
   page +=         "<div class='col-md-2'><h4 class ='text-left' style='color:white;'>Status: ";
-  page +=           "<input id='toggle-status' type='checkbox' checked data-toggle='toggle' data-onstyle='success' data-offstyle='danger' data-size='small' onchange='handleStatus(this.checked)'>";
+  page +=           "<input id='toggle-status' type='checkbox' checked data-toggle='toggle' data-onstyle='success' data-offstyle='danger' data-size='small' onchange=\"request('status', this.checked)\">";
   page +=         "</div>";
   page +=       "</div>";
   page +=       "<br>";
@@ -59,14 +61,14 @@ String getPage(){
   page +=           "<span id='slider-value' class='badge'>";
   page +=             brightnessPercentage;
   page +=           "</span></h4>";
-  page +=          "<input type='range' style='width:40%' min='0' max='100' value='"+(String)brightnessPercentage+"' class='slider' id='slider-brightness' onchange='sendBrightness(this.value)'/>";
+  page +=          "<input type='range' style='width:40%' min='0' max='100' value='"+(String)brightnessPercentage+"' class='slider' id='slider-brightness' onchange=\"request('brightness', this.value)\"/>";
   page +=         "</div>";
   page +=       "</div>";
   page +=       "<br>";
   page +=       "<div class = 'row'>";
   page +=         "<div class='col-md-6'><h4 class ='text-left' style='color:white;'> Color: ";
   page +=           "<span id='color-picker-value' class='badge'>";
-  page +=             "<input type='color' name='color' id='color-picker' value='"+color+"' onchange='changeColor(this.value)' />";
+  page +=             "<input class='jscolor' name='color' id='color-picker' value='"+color+"' onchange=\"request('color', this.value)\" />";
   page +=           "</span></h4>";  
   page +=         "</div>";
   page +=       "</div>";
@@ -78,10 +80,10 @@ String getPage(){
   page +=                   "Effects";
   page +=               "</button>";
   page +=               "<div class='dropdown-menu' aria-labelledby='dropdownEffectsButton'>";
-  page +=                 "<a class='dropdown-item' onclick=\"handleEffect('rainbowcycle')\"> Rainbow Cycle </a>";
-  page +=                 "<a class='dropdown-item' onclick=\"handleEffect('fixedcolor')\"> Fixed Color</a>";  
-  page +=                 "<a class='dropdown-item' onclick=\"handleEffect('bpm')\"> Bpm </a>";
-  page +=                 "<a class='dropdown-item' onclick=\"handleEffect('juggle')\"> Juggle</a>";
+  page +=                 "<a class='dropdown-item' onclick=\"request('effect','rainbowcycle')\"> Rainbow Cycle </a>";
+  page +=                 "<a class='dropdown-item' onclick=\"request('effect','fixedcolor')\"> Fixed Color</a>";  
+  page +=                 "<a class='dropdown-item' onclick=\"request('effect','bpm')\"> Bpm </a>";
+  page +=                 "<a class='dropdown-item' onclick=\"request('effect','juggle')\"> Juggle</a>";
   page +=               "</div>";
   page +=            "</div>";
                       if (effect == "rainbowcycle"){
@@ -89,47 +91,61 @@ String getPage(){
   page +=               "<span id='slider-rainbow-speed-value' class='badge'>";
   page +=                 FRAMES_PER_SECOND;
   page +=               "</span></h4>";
-  page +=               "<input type='range' style='width:40%' min='5' max='1000' value='"+(String)FRAMES_PER_SECOND+"' class='slider' id='slider-rainbow-speed' onchange='handleBrightnessSpeed(this.value)'/>";                      
+  page +=               "<input type='range' style='width:40%' min='5' max='1000' value='"+(String)FRAMES_PER_SECOND+"' class='slider' id='slider-rainbow-speed' onchange=\"request('rainbow_speed',this.value)\"/>";                      
                       }
   page +=         "</div>";
   page +=         "</div>";
+  page +=       "</div>";
+  page +=     "<br><br>";
+  page +=       "<div>";
+  page +=       "<table style='width: 100%; color:white;'>";
+  page +=         "<tr>";
+  page +=           "<th>";
+  page +=             "Fan Nr";
+  page +=           "</th>";
+  page +=           "<th>";
+  page +=             "Status";
+  page +=           "</th>";
+  page +=           "<th>";
+  page +=             "Color";
+  page +=           "</th>";
+  page +=         "</tr>";
+  for(int i=0; i<=NUM_LEDS-1; i++){
+     page +=         "<tr>";
+     page +=          "<td>";
+     page +=          i+1;
+     page +=          "</td>";
+     page +=          "<td>";
+     page +=            "TO DO";
+     page +=          "</td>";
+     page +=          "<td>";
+     page +=             "<span id='color-picker-value' class='badge'>";
+     page +=               "<input class='jscolor' name='color' id='"+(String)i+"' value='"+color+"' onchange=\"requestSingleFan('color-single-fan', this, this.value)\" />";
+     page +=             "</span>";
+     page +=          "</td>";
+     page +=         "</tr>";
+  }
+  page +=         "<tr>";
+  page +=         "</tr>";
+  page +=       "</table>";
   page +=       "</div>";
   page +=     "<br><br><p><a href='https://github.com/petealves' target='_blank'><i style='font-size:35px' class='fab'>&#xf09b;</i></p>";
   page += "</div></div></div>";
   page += "<script>";
 
-  page +=       "function handleStatus(value){";
+  page +=       "function request(key, value){";
   page +=       "var request = new XMLHttpRequest();";
+  page +=       "if(key === 'brightness'){ ";
+  page +=       "document.getElementById('slider-value').innerHTML = value;} ";
   page +=       "request.open('POST', '/');";
   page +=       "request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');";
-  page +=       "request.send(encodeURI('status')+'='+value);}"; 
-  
-  page +=       "function sendBrightness(value){";
-  page +=       "document.getElementById('slider-value').innerHTML = value; ";
-  page +=       "var request = new XMLHttpRequest();";
-  page +=       "request.open('POST', '/');";
-  page +=       "request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');";
-  page +=       "request.send(encodeURI('brightness')+'='+value);}"; 
-   
-  page +=       "function changeColor(value){";
-  page +=       "var request = new XMLHttpRequest();";
-  page +=       "request.open('POST', '/');";
-  page +=       "request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');";
-  page +=       "request.send(encodeURI('color')+'='+value);}";  
-  
-  page +=       "function handleEffect(effect){";
-  page +=       "var request = new XMLHttpRequest();";
-  page +=       "request.open('POST', '/');";
-  page +=       "request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');";
-  page +=       "request.send(encodeURI('effect')+'='+effect);";
-  page +=       "}";  
+  page +=       "request.send(encodeURI(key)+'='+value);}";
 
-  page +=       "function handleBrightnessSpeed(value){";
-  page +=       "document.getElementById('slider-rainbow-speed-value').innerHTML = value; ";
+  page +=       "function requestSingleFan(key, row, value){";
   page +=       "var request = new XMLHttpRequest();";
   page +=       "request.open('POST', '/');";
   page +=       "request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');";
-  page +=       "request.send(encodeURI('rainbow_speed')+'='+value);}"; 
+  page +=       "request.send(encodeURI(key)+'='+value+'&fanNr='+row.id);};";
   
   page += "</script>";
   page += "</body></html>";
@@ -148,7 +164,15 @@ void handleRoot() {
 
   if (server.hasArg("color")){
     color = server.arg("color");
+    //Serial.println(color);
     handleColor();
+  }
+
+  if (server.hasArg("color-single-fan")){
+    
+    if (server.hasArg("fanNr")){
+      handleColorSingleFan(server.arg("color-single-fan"), server.arg("fanNr").toInt());
+    }
   }
 
   if (server.hasArg("effect")){
@@ -283,10 +307,15 @@ void setup() {
   server.begin();
   Serial.println("HTTP server started");
 
-  for(int i=0; i<=NUM_LEDS-1; i++){
-    leds[i] = CRGB::Green;
-  }
-  FastLED.show();
+  loadColor();
+  //handleColor();
+  
+    /*for(int i=0; i<=NUM_LEDS-1; i++){
+      leds[i] = CRGB::Red;
+    }*/
+  
+  //FastLED.show()
+  
 }
 
 
@@ -315,7 +344,7 @@ void loop() {
     FastLED.delay(2000/FRAMES_PER_SECOND); 
     EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
   }
-  delay(10);
+  delay(1);
 }
 
 void handleColor() {
@@ -324,9 +353,8 @@ void handleColor() {
   juggleEffect = 0;
   bpmEffect = 0;
   
-  
   String colorReplaced = color;
-  colorReplaced.remove(0, 1); // Remove first char (#)
+  //colorReplaced.remove(0, 1); // Remove first char (#)
   long number = (long) strtol( &colorReplaced[0], NULL, 16);
   int l_r = number >> 16;
   int l_g = number >> 8 & 0xFF;
@@ -335,9 +363,10 @@ void handleColor() {
   Serial.println(l_r);
   Serial.println(l_g);
   Serial.println(l_b);
-
+  saveColor(colorReplaced);
   updateColor(l_r, l_g, l_b);
 }
+
 
 void updateColor(int l_r, int l_g, int l_b) {
   r = l_r;
@@ -348,7 +377,23 @@ void updateColor(int l_r, int l_g, int l_b) {
   }
     
   FastLED.show();
+}
 
+void handleColorSingleFan(String colorReplaced, int fanNr) {
+
+  rainbowCycle = 0;
+  juggleEffect = 0;
+  bpmEffect = 0;
+    
+  //String colorReplaced = color;
+
+  long number = (long) strtol( &colorReplaced[0], NULL, 16);
+  int l_r = number >> 16;
+  int l_g = number >> 8 & 0xFF;
+  int l_b = number & 0xFF;
+  
+  leds[fanNr] = CRGB(l_r, l_g, l_b);
+  FastLED.show();
 }
 
 void handleJuggle(){
@@ -368,4 +413,44 @@ void handleBpm(){
   for( int i = 0; i < NUM_LEDS; i++) { //9948
     leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
   }
+}
+
+void saveColor(String colorSaved) {
+  Serial.print("Color to save:");
+  Serial.println(colorSaved);
+  EEPROM.begin(512);
+  int _size = colorSaved.length();
+  int i;
+  for(i=0;i<_size;i++)
+  {
+    EEPROM.write(0+i,colorSaved[i]);
+  }
+  EEPROM.write(0+_size,'\0');   //Add termination null character for String Data
+  EEPROM.commit();
+  EEPROM.end();
+}
+
+void loadColor() {
+  String colorSaved = "";
+  EEPROM.begin(512);
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(0);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(0+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  EEPROM.end();
+  Serial.println("Recovered credentials:");
+  Serial.println(data);
+  String dataStr(data);
+  if (dataStr.length() == 6){
+    color = data;
+  }
+  handleColor();
 }
